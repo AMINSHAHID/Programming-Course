2cb80887dabe8a9cc94536f9de53f9159e5fcced
MuzammilRafay <muzammil.rafay@gmail.com>
2021-03-28 18:39:38 +0500

    second class of javascript

---
+++ 2-JAVASCRIPT/1-First-class/app.js
@@ -0,0 +1,55 @@
+// alert("working");
+
+// console.log("this is a console."); //text
+// console.log(12345); //number
+// console.log(null);
+// console.log(true); // boolean
+// console.log(false); // boolean
+// console.log([1, 2, 3, 4, 5]); // array
+// console.log({ a: 1, b: 2 });
+// console.table({ a: 1, b: 2 });
+// console.warn("this is a error");
+// console.time();
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+// console.log("hello world");
+// console.timeEnd();
+// console.error("this is a error");
+
+// DATAYPES
+
+//PRIMITVE TYPE
+//store directly in the location the variable accesses stored on the stack.
+var store = "javascript second class"; //String
+var store = 12345; // Number
+var store = true; // Boolean // 0 false 1 true
+var store = false; // boolean
+var store = null; // null
+var store = undefined; // undefined
+var store = Symbol("values of symbol");
+console.log(store, "store one");
+
+//REFERENCE TYPE
+//Accessed by reference Objects that are stored on the heap a pointer to a location in memory
+var store = ["one", "two", "three", "four"]; // array
+var store = { a: 1, b: 2, c: 3 }; //object literals
+// var store = [1, 2, 3, 4, 5];
+console.log(store, "store second");
+// console.table(store);
+
+//case sensitive
+var secondValue = "mudassir";
+var secondValue = "muzammil";
+console.log(secondValue, "secondValue");
+console.log(secondvalue, "secondvalue");
+
+//naming of variables
+//letters,underscore,camelcase,pascal case
+
+var naming = "working"; //letters
+var naming_variable = "working"; //Underscore
+var namingVariable = "working"; //Camel Case
+var NamingVariable = "working"; //pascal Case
